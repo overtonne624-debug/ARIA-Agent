e5a3599013c5715a19a753c6607f7c639fc40bb5
ashutosh raj <overtonne624@gmail.com>
2026-07-25 18:01:55 +0530

    fix CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -38,7 +38,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://aria-agent-seven.vercel.app",
+        "http://127.0.0.1:5173",
+        "https://aria-agent-seven.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
